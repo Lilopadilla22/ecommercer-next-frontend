1559568e8cd2f10ab80ea24b9e9794ffb00e2f40
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-06 18:53:04 -0500

    add info the account

---
+++ app/account/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../layout/layout'
+import Info from '../components/Account/info/Info'
+
+export default function Account() {
+  return (
+    <>
+        <Layout isContainer relative>
+            <Info/>
+        </Layout>
+    </>
+  )
+}
