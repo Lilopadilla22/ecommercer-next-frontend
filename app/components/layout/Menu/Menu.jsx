db83b3b58f77e8f2042302070d10c6a1409c5916
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-06 17:16:25 -0500

    add menu icon and title

--- app/components/layout/Menu/Menu.jsx
+++ app/components/layout/Menu/Menu.jsx
@@ -2,7 +2,9 @@
 import {Image, Icon, Input} from 'semantic-ui-react'
 import { useEffect, useState } from 'react'
 import { Platform } from '@/app/api'
+import { map } from 'lodash'
 import styles from './Menu.module.scss'
+import Link from 'next/link'
 
 
 const platformCtrl = new Platform
@@ -26,8 +28,13 @@ export default function Menu({isOpenSearch}) {
 
 
     return (
-        <div>
-            HOLA SOY EN MENU
+        <div className={styles.platforms}>
+            {map(platforms, (platform) => (
+                <Link key={platform.id} href={`/games/${platform.attributes.slug}`}>
+                    <Image src={platform.attributes.icon.data.attributes.url}/>
+                    {platform.attributes.title}
+                </Link>
+            ))}
         </div>
     )
 }
