a96fa060c106d1a82222a7d68efc57791eac22f9
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-06 21:31:57 -0500

    add logic update email

---
+++ app/components/Account/Settings/ChangeEmailForm/ChangeEmailForm.tsx
@@ -0,0 +1,51 @@
+import { Form } from 'semantic-ui-react'
+import styles from './ChangeEmailForm.module.scss'
+import { useFormik } from 'formik'
+import { initialValues, validateSchema } from './ChangeEmailForm.form'
+import { User } from '@/app/api'
+import { useAuth } from '@/app/hook'
+
+const userCtrl = new User()
+
+export default function ChangeEmailForm() {
+
+  const {user, updateUser} = useAuth()
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: validateSchema(),
+    validateOnChange: false,
+    onSubmit: async (formValue) => {
+      try {
+        await userCtrl.updateMe(user.id, {email: formValue.email})
+        updateUser('email', formValue.email)
+        formik.handleReset()
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  })
+
+  return (
+    <Form className={styles.form} onSubmit={formik.handleSubmit}>
+      <label>Cambiar correo electronico</label>
+      <Form.Input
+        name='email'
+        placeholder='Nuevo correo electronico'
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        error={formik.errors.email}
+      />
+      <Form.Input
+        name='repeatEmail'
+        placeholder='Repetir correo electronico'
+        value={formik.values.repeatEmail}
+        onChange={formik.handleChange}
+        error={formik.errors.repeatEmail}
+      />
+      <Form.Button type='submit' loading={formik.isSubmitting}>
+        Enviar
+      </Form.Button>
+    </Form>
+  )
+}
