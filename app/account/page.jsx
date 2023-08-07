815366f4fee86bd77084a992b0d1f239ba23ad3f
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-06 19:18:03 -0500

    add component Tab in Account

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
+'use client'
 import Layout from '../layout/layout'
 import Info from '../components/Account/info/Info'
+import { Tab } from 'semantic-ui-react'
+import styles from './account.module.scss'
+import { useAuth } from '../hook'
+import { useRouter } from 'next/navigation'
 
 export default function Account() {
+
+  const {logout, user} = useAuth()
+
+  const router = useRouter()
+
+  if(!user) {
+    router.push("/join/sign-in")
+    return null
+  } 
+
+  const panes = [
+    {
+      menuItem: 'Mis pedidos',
+      render: () => (
+        <Tab.Pane attached={false}>
+            <p>Mis pedidos</p>
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: 'Lista de deseos',
+      render: () => (
+        <Tab.Pane attached={false}>
+            <p>Mi ista de deseos</p>
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: 'Direcciones',
+      render: () => (
+        <Tab.Pane attached={false}>
+            <p>Mis direcciones</p>
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: {
+        icon: 'setting',
+        content: 'Ajustes'
+      },
+      render: () => (
+        <Tab.Pane attached={false}>
+            <p>Configuraciones</p>
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: {
+        icon: 'logout',
+        content: "",
+        onClick: logout
+      }
+    }
+  ]
+
   return (
     <>
         <Layout isContainer relative>
             <Info/>
+            <Tab 
+              menu={{secondary: true, pointing: true }} 
+              panes={panes} 
+              className={styles.Tabs} /> 
         </Layout>
     </>
   )
