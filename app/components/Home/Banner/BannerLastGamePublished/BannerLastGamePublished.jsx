99c124f9dd0ba842ea70dd408e944734b060b94e
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-07 22:55:37 -0500

    add styles banner home with info

--- app/components/Home/Banner/BannerLastGamePublished/BannerLastGamePublished.jsx
+++ app/components/Home/Banner/BannerLastGamePublished/BannerLastGamePublished.jsx
@@ -2,8 +2,10 @@
 import React, { useEffect, useState } from 'react'
 import { Game } from '../../../../api'
 import { Container, Image } from 'semantic-ui-react'
+import {DateTime} from 'luxon'
 import styles from './BannerLastGamePublished.module.scss'
 import { ENV } from '../../../../utils'
+import Link from 'next/link'
 
 const gameCtrl = new Game()
 
@@ -25,10 +27,29 @@ export default function BannerLastGamePublished() {
   if(!game) return null
 
   const wallpaper = game.attributes.wallpaper
+  const releaseDate = new Date(game.attributes.releaseDate).toISOString()
   
   return (
     <div className={styles.container}>
-      <Image src={`${ENV.SERVER_HOST}${wallpaper.data.attributes.url}`}/>
+      <Image src={`${ENV.SERVER_HOST}${wallpaper.data.attributes.url}`} className={styles.wallpaper}/>
+      <Link className={styles.infoContainer} href={game.attributes.slug}>
+        <Container>
+          <span className={styles.date}>
+            {DateTime.fromISO(releaseDate).minus({days: 1}).toRelative()}
+          </span>
+          <h2>
+            {game.attributes.title}
+          </h2>
+          <p className={styles.price}>
+            <label>
+              -40%
+            </label>
+            <span className={styles.finalPrice}>
+              37$
+            </span>
+          </p>
+        </Container>
+      </Link>
 
     </div>
   )
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,10 +1,12 @@
 import Layout from "../layout/layout";
 import {HomeComponent} from '../components/Home/Banner/index'
+import Separator from "../components/Shared/Separator/Separator";
 
 export default function Home() {
   return (
     <Layout container relative>
       <HomeComponent.BannerLastGamePublished/>
+      <Separator height={100}/>
     </Layout>
   )
 }
