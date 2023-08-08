7e8ec42ea8af189ec0a81a90b3bdf07bc515f2ea
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-08 11:53:48 -0500

    add component BannerAd in home

---
+++ app/components/Shared/BannerAd/BannerAd.jsx
@@ -0,0 +1,22 @@
+'use client'
+import { Button, Container, Image } from 'semantic-ui-react'
+import styles from './BannerAd.module.scss'
+import Link from 'next/link'
+
+export function BannerAd({title, subtitle, btnTitle, btnLink, image}) {
+
+  return (
+    <div className={styles.container}>
+        <Container className={styles.containerImage}>
+            <Image src={image}/>
+        </Container>
+        <div className={styles.infoContainer}>
+            <Container>
+                <h1>{title}</h1>
+                <h3>{subtitle}</h3>
+                <Button as={Link} href={btnLink} primary>{btnTitle}</Button>
+            </Container>
+        </div>
+    </div>
+  )
+}
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -2,7 +2,8 @@
 import Layout from "../layout/layout";
 import {HomeComponent} from '../components/Home/Banner/index'
 import Separator from "../components/Shared/Separator/Separator";
-import {BarTrust} from '../components/Shared/BarTrust/BarTrust'
+import { BarTrust } from '../components/Shared/BarTrust/BarTrust'
+import { BannerAd } from '../components/Shared/BannerAd/BannerAd'
 import { Container } from "semantic-ui-react";
 
 const platFormId = {
@@ -30,6 +31,22 @@ export default function Home() {
         <HomeComponent.Latest title='PlayStation' limit={3} platformId={platFormId.playStation}/>
       </Container>
       
+      <Separator height={100}/>
+
+      <BannerAd 
+        title="Registrate y obten los mejores precios!"  
+        subtitle= '¡Compara con otros juegos y elige el tuyo'
+        btnTitle='Entra Ahora'
+        btnLink= '/account'
+        image= '/images/img01.png'
+      />
+
+      <Separator height={50}/>
+
+      <Container>
+        <HomeComponent.Latest title='Xbox' limit={3} platformId={platFormId.xbox}/>
+      </Container>
+
       <Separator height={100}/>
 
     </Layout>
