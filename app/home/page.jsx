855657ca018d1aec25a191a7221a46c87ebe98e9
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-08 11:26:25 -0500

    Add banner games in home

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -5,6 +5,13 @@ import Separator from "../components/Shared/Separator/Separator";
 import {BarTrust} from '../components/Shared/BarTrust/BarTrust'
 import { Container } from "semantic-ui-react";
 
+const platFormId = {
+  playStation: 1,
+  xbox: 2,
+  nintendo: 3,
+  pc: 4
+} 
+
 export default function Home() {
   return (
     <Layout container relative>
@@ -18,7 +25,12 @@ export default function Home() {
       <Separator height={100}/>
         <BarTrust/>
       <Separator height={100}/>
+
+      <Container>
+        <HomeComponent.Latest title='PlayStation' limit={3} platformId={platFormId.playStation}/>
+      </Container>
       
+      <Separator height={100}/>
 
     </Layout>
   )
