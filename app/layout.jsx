76ad49c6871c2633cba447246cc0427de3530f59
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-19 10:15:39 -0500

    add library for carrusel

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,8 @@ import { AuthProvider } from './context/AuthContext';
 import 'semantic-ui-css/semantic.min.css'
 import { Inter } from 'next/font/google'
 import "./scss/global.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 // @/app/scss/global.scss
 
