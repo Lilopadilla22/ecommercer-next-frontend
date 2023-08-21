6b64dcba17a2edc437d08d8c5976ab67c72dbb72
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-20 20:56:55 -0500

    add multiple products to cart

--- app/api/cart.jsx
+++ app/api/cart.jsx
@@ -2,8 +2,20 @@ import { ENV, authFetch } from '../utils'
 import { size } from 'lodash'
 
 export class Cart {
+
     add(gameId) {
-        localStorage.setItem( ENV.CART, JSON.stringify([gameId]))
+
+        const games = this.getAll()
+        const objIndex = games.findIndex((game) => game.id === gameId)
+
+        if(objIndex < 0) {
+            games.push({ id: gameId, quantity: 1 })
+        } else {
+            const game = games[objIndex]
+            games[objIndex].quantity = game.quantity + 1
+        }
+
+        localStorage.setItem( ENV.CART, JSON.stringify(games))
     }
 
     getAll () {
