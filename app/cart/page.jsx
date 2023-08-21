c3cef7f71b8c1af7c6afdadd4457f83d652e1c76
Lilia Padilla <lilopadilla22@gmail.com>
2023-08-20 23:19:18 -0500

    add logic item cart

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+'use client'
+import { useSearchParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import { useCart } from '../hook'
+import { Game } from '../api'
+
+const gameCtrl = new Game
 
 export default function page() {
+
+  const [games, setGames] = useState(null)
+
+  const{ cart } = useCart()
+  const step = useSearchParams().get('step')
+  const currentStep = Number(step)
+
+
+  useEffect(() => {
+    (async() => {
+      try {
+        const data= []
+        for await ( const item of cart) {
+          const response = await gameCtrl.getGameById(item.id)
+          data.push({...response.data, quantity: item.quantity})
+          console.log(response, 'QUE ESESTO🤷‍♀️')
+        }
+        console.log(data, 'DATAAAAAA')
+        setGames(data)
+      } catch (error) {
+        console.error(error)
+      }
+    })()
+  }, [cart])
+  
+
   return (
-    <div>
-      Soy el carrito
-    </div>
+    <>
+      { currentStep === 1 && <p>StepOne</p> }
+      { currentStep === 2 && <p>StepTwo</p> }
+      { currentStep === 3 && <p>stepThree</p> }
+    </>
   )
+  
 }
